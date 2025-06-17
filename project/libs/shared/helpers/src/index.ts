796c9905b6f2b8fcadfd20da2b688c4ad970e45e
Tanya <lee-tanya2insp@yandex.ru>
2025-06-17 09:41:04 +0300

    email-subscriber

--- project/libs/shared/helpers/src/index.ts
+++ project/libs/shared/helpers/src/index.ts
@@ -1,4 +1,3 @@
-export { fillDto } from './lib/common';
 export { getMongoConnectionString } from './lib/common';
 export { getRabbitMQOptions } from './lib/brokers';
-
+export { getMailerAsyncOptions } from './lib/mail';
